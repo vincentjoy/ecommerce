c90246aeef42ed7e64a052e665db0c33f2ed752c
Vincent Joy <vincentjoyhere@gmail.com>
2025-01-27 21:01:22 +0530

    added the router for the category view set

--- drfecommerce/drfecommerce/urls.py
+++ drfecommerce/drfecommerce/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+from drfecommerce.apps.product import views
+
+router = DefaultRouter()
+router.register(r'categoy', views.CategoryView)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
