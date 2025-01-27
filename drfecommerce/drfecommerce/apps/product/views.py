3680bdd2476f952f360df004cd3c2960ea9497d1
Vincent Joy <vincentjoyhere@gmail.com>
2025-01-27 20:39:33 +0530

    added the category view set

--- drfecommerce/drfecommerce/apps/product/views.py
+++ drfecommerce/drfecommerce/apps/product/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+from .models import Category
+from .serializers import CategorySerializer
 
-# Create your views here.
+
+class CategoryView(viewsets.ViewSet):
+    """
+    A simple view set for viewing categories
+    """
+
+    queryset = Category.objects.all()
+
+    def list(self, request):
+        queryset = self.get_queryset()
+        serializer = CategorySerializer(queryset, many=True)
+        return Response(serializer.data)
