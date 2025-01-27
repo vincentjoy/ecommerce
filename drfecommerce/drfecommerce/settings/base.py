1f01662df65738bb402c85317b183c14173146c0
Vincent Joy <vincentjoyhere@gmail.com>
2025-01-27 15:33:21 +0530

    created the product model and migrated it

--- drfecommerce/drfecommerce/settings/base.py
+++ drfecommerce/drfecommerce/settings/base.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # External packages
-    'rest_framework'
+    'rest_framework',
     # Installed apps
     'drfecommerce.apps.product',
 ]
