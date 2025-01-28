aaa7b4bc4691657c1544990ff5a79a5c22eb7614
Vincent Joy <vincentjoyhere@gmail.com>
2025-01-28 16:18:30 +0530

    added the UI for the schema with Swagger

--- drfecommerce/drfecommerce/urls.py
+++ drfecommerce/drfecommerce/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 from drfecommerce.apps.product import views
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 router = DefaultRouter()
 router.register(r'categoy', views.CategoryView)
@@ -25,4 +26,6 @@ router.register(r'categoy', views.CategoryView)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    path('api/schema', SpectacularAPIView.as_view(), name="schema"),
+    path('api/schema/docs', SpectacularSwaggerView.as_view(url_name="schema")),
 ]
